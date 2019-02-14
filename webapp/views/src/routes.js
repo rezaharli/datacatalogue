5ce37a2839014a9a6da3fdccfaa6d563e5f9cc39
Reza Harli <reza.saputra@eaciit.com>
2019-02-14 10:12:09 +0700

    asdf

--- webapp/views/src/routes.js
+++ webapp/views/src/routes.js
@@ -117,8 +117,6 @@ router.beforeEach((to, from, next) => {
   const publicPages = ['/login'];
   const authRequired = !publicPages.includes(to.path);
   const loggedIn = localStorage.getItem('user');
-
-  console.log(localStorage.getItem('user'));
   
   if (authRequired && !loggedIn) {
     return next('/login');
