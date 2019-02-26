2f2b2872d4cdc381d6dfe5d96aa8d5aab59e779c
Reza Harli <reza.saputra@eaciit.com>
2019-02-26 18:09:45 +0700

    cicd

---
+++ build_linux.sh
@@ -0,0 +1,24 @@
+export GOOS=linux
+
+foldername="$CI_PROJECT_NAME-$CI_COMMIT_TAG"
+rm -r "builds"
+mkdir "builds/${foldername}"
+mkdir "builds/${foldername}/webapp"
+mkdir "builds/${foldername}/webapp/config"
+mkdir "builds/${foldername}/webapp/views"
+
+cd webapp
+go build -o datacatalogue
+mv datacatalogue "../builds/${foldername}/webapp"
+
+cd views
+yarn build
+cp -r dist/ "../../builds/${foldername}/"
+
+cd ..
+cp config/app.json.template "../builds/${foldername}/webapp/config/"
+cd ../builds/
+tar -zcf "${foldername}.tar.gz" "${foldername}"
+rm -r "${foldername}"
+
+export GOOS=darwin
