a27fd102ba1669cbbe77ee3661386342dce49f65
Reza Harli <reza.saputra@eaciit.com>
2019-03-08 02:31:04 +0700

    Hover link to show full text

--- webapp/views/src/components/Tablecell.vue
+++ webapp/views/src/components/Tablecell.vue
@@ -1,27 +1,33 @@
 <template>
   <div>
     <span v-if="isklik" @click="toggleText">{{ isTruncated ? truncateCells(fulltext) : fulltext }}</span>
-    <span v-if="!isklik">{{ isTruncated ? truncateCells(fulltext) : fulltext }}</span>
+
+    <p
+      style="margin-bottom: 0px !important;"
+      v-if="!isklik"
+      @mouseenter="toggleText"
+      @mouseleave="toggleText"
+    >{{ isTruncated ? truncateCells(fulltext) : fulltext }}</p>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "tablecell",
-    props: ["fulltext", "isklik"],
-    data () {
-      return {
-        isTruncated: true,
-      }
+export default {
+  name: "tablecell",
+  props: ["fulltext", "isklik"],
+  data() {
+    return {
+      isTruncated: true
+    };
+  },
+  methods: {
+    toggleText() {
+      this.isTruncated = !this.isTruncated;
     },
-    methods: {
-      toggleText(){
-        this.isTruncated = !this.isTruncated
-      },
-      truncateCells(text){
-        var n = 15;
-        return text.length > n ? text.slice(0, n) + "..." : text;
-      }
+    truncateCells(text) {
+      var n = 15;
+      return text.length > n ? text.slice(0, n) + "..." : text;
     }
   }
+};
 </script>
