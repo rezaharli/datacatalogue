9223fafd2839841152857fd19651c4bd82a3d85c
Reza Harli <reza.saputra@eaciit.com>
2019-03-25 02:30:07 +0800

    Bring back refresh table. pikir keri.

--- webapp/views/src/components/dsc/Dsc-interfaces.vue
+++ webapp/views/src/components/dsc/Dsc-interfaces.vue
@@ -234,6 +234,10 @@ export default {
         if (to.params != undefined) {
           this.store.isRightTable = to.params.system; 
         }
+
+        if(this.store.isRightTable){
+          this.doGetRightTable(this.$route.params.system);
+        }
       },
       "store.left.pagination": {
         handler () {
