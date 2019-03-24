15cc2c0707297746b0a0056d9735fb5afb067df5
Reza Harli <reza.saputra@eaciit.com>
2019-03-25 01:27:07 +0800

    update model

--- webapp/models/DSProcessDetail.go
+++ webapp/models/DSProcessDetail.go
@@ -3,8 +3,8 @@ package models
 type DSProcessDetail struct {
 	ID                        int
 	Process_ID                int
-	Column_ID                 int
-	Segment_Name              string
+	Business_Term_ID          int
+	Segment_ID                int
 	Imm_Prec_System_ID        int
 	Ultimate_Source_System_ID int
 }
