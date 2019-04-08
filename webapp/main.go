8cf67be6240eeb9e3632a8ea37b11f6bd97baf49
Reza Harli <reza.saputra@eaciit.com>
2019-04-08 21:26:20 +0700

    Enable data dummy + dist

--- webapp/main.go
+++ webapp/main.go
@@ -24,28 +24,6 @@ func main() {
 	}
 	defer clit.Close()
 
-	// s.NewDSCService().CreateSystemDummyData()
-	// s.NewDSCService().CreatePolicyDummyData()
-	// s.NewDSCService().CreateCategoryDummyData()
-	// s.NewDSCService().CreateSubCategoryDummyData()
-	// s.NewDSCService().CreateBusinessTermDummyData()
-	// s.NewDSCService().CreateMDResourceDummyData()
-	// s.NewDSCService().CreateMDTableDummyData()
-	// s.NewDSCService().CreateMDColumnDummyData()
-	// s.NewDSCService().CreatePeopleDummyData()
-	s.NewDSCService().CreateUserDummyData()
-	// s.NewDSCService().CreateDSProcessesDummyData()
-	// s.NewDSCService().CreateSegmentDummyData()
-	// s.NewDSCService().CreateDSProcessesDetailDummyData()
-	// s.NewDSCService().CreateLinkSubcategoryPeopleDummyData()
-	// s.NewDSCService().CreateLinkCategoryPeopleDummyData()
-	// s.NewDSCService().CreatePriorityReportsDummyData()
-	// s.NewDSCService().CreateCRMDummyData()
-	// s.NewDSCService().CreateLinkCRMCDEDummyData()
-	// s.NewDSCService().CreateRoleDummyData()
-	// s.NewDSCService().CreateLinkRolePeopleDummyData()
-	// s.NewDSCService().CreateLinkColumnInterfaceDummyData()
-
 	app := knot.NewApp()
 	app.Register(c.NewUsersController(), "")
 	app.Register(c.NewDSCController(), "")
@@ -57,13 +35,13 @@ func main() {
 	app.Static("js", filepath.Join(clit.ExeDir(), "views", "dist", "js"))
 	app.Static("img", filepath.Join(clit.ExeDir(), "views", "dist", "img"))
 
-	s := knot.NewServer()
-	s.RegisterApp(app, "")
+	server := knot.NewServer()
+	server.RegisterApp(app, "")
 
 	fmt.Println("environment", clit.Config("default", "environment", "").(string))
 	switch clit.Config("default", "environment", "").(string) {
 	case "prod", "production":
-		s.Route("/", func(ctx *knot.WebContext) {
+		server.Route("/", func(ctx *knot.WebContext) {
 			ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 			ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
@@ -73,11 +51,34 @@ func main() {
 			http.ServeFile(ctx.Writer, ctx.Request, htmlPath)
 		})
 	case "dev", "development":
-		s.ReverseProxy("/", "http://localhost:8080", knot.VirtualDirectory)
+		server.ReverseProxy("/", "http://localhost:8080", knot.VirtualDirectory)
 	}
 
-	s.Start(clit.Config("default", "host", "").(string) + ":" + clit.Config("default", "port", "").(string))
-	s.Wait()
+	server.Start(clit.Config("default", "host", "").(string) + ":" + clit.Config("default", "port", "").(string))
+
+	s.NewDSCService().CreateSystemDummyData()
+	s.NewDSCService().CreatePolicyDummyData()
+	s.NewDSCService().CreateCategoryDummyData()
+	s.NewDSCService().CreateSubCategoryDummyData()
+	s.NewDSCService().CreateBusinessTermDummyData()
+	s.NewDSCService().CreateMDResourceDummyData()
+	s.NewDSCService().CreateMDTableDummyData()
+	s.NewDSCService().CreateMDColumnDummyData()
+	s.NewDSCService().CreatePeopleDummyData()
+	s.NewDSCService().CreateUserDummyData()
+	s.NewDSCService().CreateDSProcessesDummyData()
+	s.NewDSCService().CreateSegmentDummyData()
+	s.NewDSCService().CreateDSProcessesDetailDummyData()
+	s.NewDSCService().CreateLinkSubcategoryPeopleDummyData()
+	s.NewDSCService().CreateLinkCategoryPeopleDummyData()
+	s.NewDSCService().CreatePriorityReportsDummyData()
+	s.NewDSCService().CreateCRMDummyData()
+	s.NewDSCService().CreateLinkCRMCDEDummyData()
+	s.NewDSCService().CreateRoleDummyData()
+	s.NewDSCService().CreateLinkRolePeopleDummyData()
+	s.NewDSCService().CreateLinkColumnInterfaceDummyData()
+
+	server.Wait()
 }
 
 func kill(err error) {
