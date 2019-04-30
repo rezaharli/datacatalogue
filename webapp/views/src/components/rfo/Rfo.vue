d380dcd10e15f47b14e9038b1a3585938704f28c
Reza Harli <reza.saputra@eaciit.com>
2019-04-30 07:30:40 +0700

    Remove unnecessary

--- webapp/views/src/components/rfo/Rfo.vue
+++ webapp/views/src/components/rfo/Rfo.vue
@@ -32,53 +32,7 @@
 
                 <b-col sm=12 md=1 lg=1 />
             </b-row>
-
-            <!-- <transition name="fade" mode="out-in">
-                <router-view></router-view>
-            </transition> -->
         </b-container>
-
-        <v-navigation-drawer v-model="store.drawer" right absolute temporary width="350">
-
-            <b-row align-h="end" class="pr-4 pt-3 bg-light-grey">
-                <b-button variant="light" class="float-right" @click.stop="store.drawer = !store.drawer">
-                    <i class="fa fa-fw fa-arrow-right"></i>
-                </b-button>
-            </b-row>
-
-            <v-list class="p-0 bg-light-grey">
-                <!-- <v-list-tile avatar>
-                    <v-list-tile-content>
-                        <v-list-tile-title>{{ store.drawerContent.systemName }}</v-list-tile-title>
-                    </v-list-tile-content>
-                    <v-list-tile-content>
-                        <v-list-tile-title>{{ store.drawerContent.itamID }}</v-list-tile-title>
-                    </v-list-tile-content>
-                    <v-list-tile-content>
-                        <v-list-tile-title>{{ store.drawerContent.owners.length }}</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile> -->
-                <h1 class="px-4 py-2">{{ store.drawerContent.systemName }}</h1>
-                <b-row class="px-4 pt-2 pb-4">
-                    <b-col cols=12 sm=auto class="border-right border-dark">ITAM ID: {{ store.drawerContent.itamID }}</b-col>
-                    <b-col cols=12 sm=auto>{{ store.drawerContent.owners.length }} Owners</b-col>
-                </b-row>
-
-            </v-list>
-            
-
-            <v-list class="px-0 pt-4">
-                <!-- <v-divider></v-divider> -->
-
-                <b-row align-v="center" class="px-4 py-2"
-                    v-for="(owner, i) in store.drawerContent.owners"
-                    :key="i">
-                    <b-col cols="3" class="text-right">{{ owner.BANK_ID }}</b-col>
-
-                    <b-col cols="9" class="text-capitalize border-left border-dark">{{ owner.DATASET_CUSTODIAN }}</b-col>
-                </b-row>
-            </v-list>
-        </v-navigation-drawer>
     </v-content>
 </template>
 
