1a9b2b4c1745f2d3e9c60ad031e93427c78bafd7
Lorensio Derrint Yulda Nugrahutama <lorensio.nugrahutama@eaciit.com>
2019-06-27 20:30:00 +0700

    314 - DSC - Data Dictionary  - Fix Alignment issue occurs when click on the Table Name or Column Name

--- webapp/views/src/components/Tablecell.vue
+++ webapp/views/src/components/Tablecell.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <span v-if="showOn == 'click'" @click="toggleText">{{ isTruncated ? truncateCells(fulltext) : fulltext }}</span>
+  <div class="wrapper-showmore d-inline-block">
+    <span v-if="showOn == 'click'" @click="toggleText(); onClick($event, isTruncated);">{{ isTruncated ? truncateCells(fulltext) : fulltext }}</span>
 
     <p
       style="margin-bottom: 0px !important;"
@@ -20,6 +20,11 @@ export default {
       isTruncated: true
     };
   },
+  mounted() {
+    setTimeout(() => {
+      this.setThWidthOri();
+    }, 1);
+  },
   methods: {
     toggleText() {
       this.isTruncated = !this.isTruncated;
@@ -30,6 +35,38 @@ export default {
       if( ! text) return "";
 
       return text.length > n ? text.slice(0, n) + "..." : text;
+    },
+    setThWidthOri() {
+      $("table.v-table > thead > tr > th").each(function () {
+        var thElem = $(this);
+        var thWidthOri = thElem.outerWidth();
+        thElem.attr("data-width-ori", thWidthOri);
+      });
+    },
+    onClick: function (e, isTruncated) {
+      var clickedElem = e.currentTarget;
+      var tdElem = $(clickedElem).closest("td");
+      var keberapa = tdElem.index();
+      setTimeout(function(){
+        var tdWidth = tdElem.outerWidth();
+        var tdWidthMax = Math.max.apply(Math, tdElem.closest("table > tbody > tr > td").eq(keberapa).map(function(){ return $(this).outerWidth(); }).get());
+        var wrapperShowMoreWidthMax = Math.max.apply(Math, tdElem.closest("table > tbody > tr > td").eq(keberapa).children(".wrapper-showmore").map(function(){ return $(this).outerWidth(); }).get());
+        var thWidth = tdElem.closest("table").children("thead").children("tr").children("th").eq(keberapa).attr("data-width-ori");
+        
+        if(isTruncated){
+          if(wrapperShowMoreWidthMax>thWidth){
+            var tdWidthUsed = tdWidthMax;
+          }else{
+            var tdWidthUsed = thWidth;
+          }
+        }else{
+          var tdWidthUsed = tdWidthMax;
+        }
+        tdElem.closest("table").children("tbody").children("tr").each(function () {
+          $(this).children("td").eq(keberapa).css({"width": tdWidthUsed+"px"});
+        });
+        tdElem.closest("table").children("thead").children("tr").children("th").eq(keberapa).css({"min-width": tdWidthUsed+"px"});
+      }, 10);
     }
   }
 };
