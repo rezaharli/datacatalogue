47960fdadc82bfac0f0e99fcdf3b38673c625476
Lorensio Derrint Yulda Nugrahutama <lorensio.nugrahutama@eaciit.com>
2019-07-23 10:40:51 +0700

    326 - RFO - Summary View - add counts to summaries title

--- webapp/views/src/components/rfo/Rfo-summary.vue
+++ webapp/views/src/components/rfo/Rfo-summary.vue
@@ -65,7 +65,7 @@ ol {
             <b-col>
               <b-card tag="article" class="mb-2">
                 <h4 class="card-title border-0 mb-0 pb-0" v-b-toggle.collapse-1>
-                  Reports
+                  Reports <span class="text-muted pl-2 small">({{store.left.display[0].PR_COUNT}})</span>
                   <i class="when-opened float-right fa fa-chevron-up"></i>
                   <i class="when-closed float-right fa fa-chevron-down"></i>
                 </h4>
@@ -86,7 +86,7 @@ ol {
             <b-col>
               <b-card tag="article" class="mb-2">
                 <h4 class="card-title border-0 mb-0 pb-0" v-b-toggle.collapse-2>
-                  Critical Risk Measures
+                  Critical Risk Measures <span class="text-muted pl-2 small">({{store.left.display[0].CRM_COUNT}})</span>
                   <i class="when-opened float-right fa fa-chevron-up"></i>
                   <i class="when-closed float-right fa fa-chevron-down"></i>
                 </h4>
@@ -107,7 +107,7 @@ ol {
             <b-col>
               <b-card tag="article" class="mb-2">
                 <h4 class="card-title border-0 mb-0 pb-0" v-b-toggle.collapse-3>
-                  Critical Data Elements 
+                  Critical Data Elements <span class="text-muted pl-2 small">({{store.left.display[0].CDE_COUNT}})</span>
                   <i class="when-opened float-right fa fa-chevron-up"></i>
                   <i class="when-closed float-right fa fa-chevron-down"></i>
                 </h4>
